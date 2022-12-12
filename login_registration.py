9b639464ff8ea14a4ed0879b1ecdc66b1e99d4a8
Bevzyk <bevz_maxim@mail.ru>
2022-12-12 22:33:51 +0300

    book_store_testing

---
+++ login_registration.py
@@ -0,0 +1,35 @@
+"""регистрация аккаунта """
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+#
+# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+# driver.get("https://practice.automationtesting.in/")
+# driver.maximize_window()
+# driver.implicitly_wait(5)
+# wait = WebDriverWait(driver, 10)
+#
+# my_account = driver.find_element_by_link_text("My Account").click()
+# reg_email = driver.find_element_by_id("reg_email")
+# reg_email.send_keys("chelovek_pavuk@mail.ru")
+# reg_password = driver.find_element_by_id("reg_password")
+# reg_password.send_keys("Buba-854-")
+# register_btn = driver.find_element_by_css_selector("input[value='Register']").click()
+# driver.quit()
+
+"""Вход в систему"""
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+#
+# driver = webdriver.Chrome(executable_path='C:/chromedriver.exe')
+# driver.get("https://practice.automationtesting.in/")
+# driver.maximize_window()
+# driver.implicitly_wait(5)
+# wait = WebDriverWait(driver, 10)
+#
+# my_account = driver.find_element_by_link_text("My Account").click()
+# login = driver.find_element_by_id("username")
+# login.send_keys("chelovek_pavuk@mail.ru")
+# password = driver.find_element_by_id("password")
+# password.send_keys("Buba-854-")
+# login_btn = driver.find_element_by_css_selector("input[value='Login']").click()
+# driver.quit()
